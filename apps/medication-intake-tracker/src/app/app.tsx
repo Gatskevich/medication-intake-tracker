91a2795a11e827bee3478009d28920389cdde561
Radion Hatskevich <rhatskevich@sagenso.com>
2024-03-27 00:37:23 +0100

    Initial commit

---
+++ apps/medication-intake-tracker/src/app/app.tsx
@@ -0,0 +1,14 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import styles from './app.module.scss';
+
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <div>
+      <NxWelcome title="medication-intake-tracker" />
+    </div>
+  );
+}
+
+export default App;
