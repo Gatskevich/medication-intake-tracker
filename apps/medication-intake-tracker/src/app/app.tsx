298be2f1be0737c9d9962efe655bd4d13dcac00b
Radion Hatskevich <rhatskevich@sagenso.com>
2024-03-31 22:57:10 +0200

    add delay to loading

--- apps/medication-intake-tracker/src/app/app.tsx
+++ apps/medication-intake-tracker/src/app/app.tsx
@@ -3,12 +3,19 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { MedicationsProvider } from "../utils/contexts/MedicationsContext";
 import { Loading } from "../components/Loading/Loading";
 
-const LazyMedicationList = lazy(
-  () => import("../pages/MedicationList/MedicationList"),
-);
-const LazyMedicationDetails = lazy(
-  () => import("../pages/MedicationDetails/MedicationDetails"),
-);
+const LazyMedicationList = lazy(() => {
+  return Promise.all([
+    import("../pages/MedicationList/MedicationList"),
+    new Promise((resolve) => setTimeout(resolve, 1500)),
+  ]).then(([moduleExports]) => moduleExports);
+});
+
+const LazyMedicationDetails = lazy(() => {
+  return Promise.all([
+    import("../pages/MedicationDetails/MedicationDetails"),
+    new Promise((resolve) => setTimeout(resolve, 1500)),
+  ]).then(([moduleExports]) => moduleExports);
+});
 
 const App = () => {
   return (
